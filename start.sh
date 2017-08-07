472982263f29c966e664bf0e8d192006ddfeb346
Eduardo Martin Rojo <emr@sanger.ac.uk>
2017-08-07 11:29:08 +0100

    Removed lines for creating the certificate, as the destination path is not defined

--- start.sh
+++ start.sh
@@ -12,12 +12,7 @@ then
 	exit 1
 fi
 
-if [ ! -f ./nginx-selfsigned.pem ]
-then
-  openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout nginx-selfsigned.key -out nginx-selfsigned.pem
-fi
-
-nohup ./fake_hmdmc.py -c nginx-selfsigned.pem -k nginx-selfsigned.key $PORT > ./log.txt 2>&1 &
+nohup ./fake_hmdmc.py -c ../shared/nginx-selfsigned.pem -k ../shared/nginx-selfsigned.key $PORT > ./log.txt 2>&1 &
 
 echo $! > ./run.pid
 echo "OK"
